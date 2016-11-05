4c5d0bdd5caa520b49270ae9f50c856dea9e5c3b
kevingo <kevingo75@gmail.com>
2016-11-05 21:15:37 +0800

    Add fetchBoard feature

--- ptt.go
+++ ptt.go
@@ -25,19 +25,32 @@ func main() {
 	flag.Parse()
 	args := flag.Args()
 	var url string
+	var board string
 
 	if len(args) == 0 {
 		url = defaultConf.CarBoardUrl
+		fmt.Println("Fetch default car board")
+		fetchSingle(url)
+	} else if len(args) == 2 && args[1] == "allpages" {
+		board = args[0]
+		url = defaultConf.BaseUrl + board + "/index.html"
+		fmt.Println("Fetch allpages " + url)
+		fetchPages(url)
+	} else if len(args) == 1 && args[0] != "allpages" {
+		board = args[0]
+		url = defaultConf.BaseUrl + board + "/index.html"
+		fmt.Println("Fetch Single Pages " + url)
+		fetchSingle(url)
 	} else {
-		board := args[0]
+		board = args[0]
 		page := args[1]
 		url = defaultConf.BaseUrl + board + "/index" + page + ".html"
+		fmt.Println("Fetch single pages with page " + url)
+		fetchSingle(url)
 	}
-
-	fetcher(url)
 }
 
-func fetcher(url string) {
+func fetchSingle(url string) {
 	resp, err := http.Get(url)
 
 	if err != nil {
@@ -51,6 +64,22 @@ func fetcher(url string) {
 	doc.Find("div.title").Each(func(i int, s *goquery.Selection) {
 		a := s.Find("a")
 		qHref, _ := a.Attr("href")
-		fmt.Println(strings.TrimSpace(s.Text()) + "\t" + getHome() + qHref)
+		fmt.Println(strings.TrimSpace(s.Text()) + "\t" + "https://www.ptt.cc" + qHref)
 	})
+}
+
+func fetchPages(url string) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(io.Reader(resp.Body))
+
+	href, _ := doc.Find("div.action-bar").Find("a.btn").Eq(3).Attr("href")
+	pages := strings.Split(href, "/")[3]
+	fmt.Println(pages)
 }
